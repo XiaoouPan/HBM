95ea138e588d8a49795b86efd1c84272fc4c50be
Xiaoou Pan <xip024@ucsd.edu>
2021-05-16 18:21:13 -0700

    Add a buffer

--- simulation.R
+++ simulation.R
@@ -6,74 +6,20 @@ library(mvtnorm)
 
 rm(list = ls())
 
-posterior_simu = function (dat, C, iter = 2000) {
-  thismodel = try(jags.model(file = "trial.txt", 
-                             data = dat, 
-                             inits = list(mu1 = rep(-0.5, dat$N),
-                                          mu2 = rep(1, dat$N),
-                                          rho = rep(0.5, dat$N),
-                                          mumix = c(-2, -2, 0),
-                                          muprec = c(1, 1, 1),
-                                          mumix2 = c(1, 5, 1),
-                                          muprec2 = c(1, 1, 1)),
-                             n.adapt = iter), silent = TRUE)
-  res.bugs = try(jags.samples(thismodel, 
-                              variable.names = c('mu1', 'mu2', 'rho', 'mumix', 'muprec', 'mumix2', 'muprec2'),
-                              n.iter = iter), silent = TRUE)
-  return (list(mu1 = matrix(res.bugs$mu1, nrow = dat$N),
-               mu2 = matrix(res.bugs$mu2, nrow = dat$N),
-               rho = matrix(res.bugs$rho, nrow = dat$N),
-               mumix = matrix(res.bugs$mumix, nrow = C),
-               muprec = matrix(res.bugs$muprec, nrow = C),
-               mumix2 = matrix(res.bugs$mumix2, nrow = C),
-               muprec2 = matrix(res.bugs$muprec2, nrow = C)))
-}
-
-summary_posterior = function (dataVal, mcmcVal) {
-  response = dataVal$response
-  activity = dataVal$activity
-  N = dataVal$N
-  ninter = dataVal$ninter
-  group = dataVal$group
-  
-  #parm
-  mu1 = mcmcVal$mu1
-  mu2 = mcmcVal$mu2
-  rho = mcmcVal$rho
-  mumix = mcmcVal$mumix
-  muprec = mcmcVal$muprec
-  mumix2 = mcmcVal$mumix2
-  muprec2 = mcmcVal$muprec2
-  
-  rst = 0
-  for (n in 1:N) {
-    p3 = dnorm(mu1[n, ], mean = mumix[group[n], ], sd = 1 / sqrt(muprec[group[n], ]), log = T)
-    p4 = dnorm(mu2[n, ], mean = mumix2[group[n], ], sd = 1 / sqrt(muprec2[group[n], ]), log = T)
-    rst = rst + ninter * (mean(p3, na.rm = T) + mean(p4, na.rm = T))
-    for (j in 1:ninter) {
-      p1 = dnorm(activity[n, j], mean = mu2[n, ], sd = 1, log = TRUE)
-      p2 = pnorm(0, mean = mu1[n, ] + rho[n, ] * (activity[n, j] - mu2[n, ]), sd = 1, log.p = TRUE)
-      if (response[n, j] == 1) {
-        p2 = 1 - p2
-      }
-      rst = rst + mean(p1, na.rm = T) + mean(p2, na.rm = T)
-    }
-  }
-  return (rst)
-}
-
-
+source('hbm.R')
 
 ninter = 20
 N = 4
 C = 3
-M = 20
+M = 1
 
+epsilon_p = 0.05
+epsilon_mu = 0.5
 p0 = 0.15
 mu0 = 3
 prob = c(0.15, 0.15, 0.15, 0.15) ## true p
 mu1 = qnorm(prob)
-mu2 = c(3, 3, 3, 3) ## true mu
+mu2 = c(3, 3, 3, 3)  ## true mu
 rho0 = 0.5
 cluster = c(1, 1, 1, 1)
 
@@ -81,8 +27,8 @@ response = matrix(0, N, ninter)
 activity = matrix(0, N, ninter)
 Z = array(0, c(N, ninter, 2)) ## underlying bivariate normal, one of them is unobservable
 Sigma = matrix(c(1, rho0, rho0, 1), 2, 2)
-cutoff = qnorm(p0)
-cutoff2 = mu0
+cutoff = qnorm(p0 + epsilon_p)
+cutoff2 = mu0 + epsilon_mu
 all_cluster = permutations(n = C, r = N, repeats.allowed = T)
 post_prob_all = post_prob_upper_all = post_prob_lower_all = matrix(0, N, M)
 post_acti_all = post_acti_upper_all = post_acti_lower_all = post_cluster_all = matrix(0, N, M)
