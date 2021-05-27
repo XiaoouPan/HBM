4d9ffd9edb183216dc320a008dd36b5d11a70c82
Xiaoou Pan <xip024@ucsd.edu>
2021-05-27 03:07:26 -0700

    Update simu

--- simu_cont.R
+++ simu_cont.R
@@ -20,9 +20,9 @@ p0 = 0.15 ## null response rate
 mu0 = 3 ## null activity level
 prob = c(0.15, 0.15, 0.15, 0.45) ## true p
 mu1 = qnorm(prob)
-mu2 = c(3, 3, 3, 5)  ## true mu
+mu2 = c(3, 3, 5, 5)  ## true mu
 rho0 = 0.5
-cluster = c(1, 1, 1, 3) ## true cluster structure
+cluster = c(1, 1, 2, 3) ## true cluster structure
 
 response = matrix(0, N, ninter)
 activity = matrix(0, N, ninter)
