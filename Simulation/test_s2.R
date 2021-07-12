a39bf28020f0ae43faa3710ee6afe27f028e95a6
Xiaoou Pan <xip024@ucsd.edu>
2021-07-12 00:53:14 -0700

    tunning for second stage

---
+++ Simulation/test_s2.R
@@ -0,0 +1,94 @@
+library(MASS)
+library(coda)
+library(rjags) 
+library(gtools)
+library(mvtnorm)
+library(tikzDevice)
+library(ggplot2)
+
+rm(list = ls())
+
+source('hbm_cont.R')
+
+ninter = 22
+N = 4
+C = 3
+M = 50
+n.adapt = 1000
+n.burn = 1000
+n.iter = 5000
+
+p0 = c(0.15, 0.15, 0.15, 0.15) ## null response rate
+mu0 = c(3, 3, 3, 3) ## null activity level
+rho0 = 0.75
+alpha = 0.026
+reject_rate = 1 - alpha ## For hypothesis testing
+
+prob = c(0.15, 0.15, 0.15, 0.15) ## true p
+acti = c(3, 3, 3, 3)  ## true activity
+mu1 = qnorm(prob) - qnorm(p0)
+mu2 = acti - mu0
+cluster = c(1, 1, 1, 1) ## true cluster structure
+
+epsilon_p_seq = seq(0.15, 0.27, by = 0.03)
+epsilon_mu_seq = seq(0.5, 0.9, by = 0.1)
+
+response = matrix(0, N, ninter)
+activity = matrix(0, N, ninter)
+Z = matrix(0, ninter, 2) ## underlying bivariate normal, one of them is unobservable
+Sigma = matrix(c(1, rho0, rho0, 1), 2, 2)
+weak1 = weak2 = weak3 = weak4 = matrix(0, 5, 5)
+strong1 = strong2 = strong3 = strong4 = matrix(0, 5, 5)
+
+pb = txtProgressBar(style = 3)
+for (m in 1:M) {
+  set.seed(m)
+  ## Data generation
+  for (i in 1:N) {
+    Z = mvrnorm(ninter, c(mu1[i], mu2[i]), Sigma)
+    response[i, ] = as.numeric(Z[, 1] > 0)
+    activity[i, ] = Z[, 2]
+  }
+  
+  ## stage 2 with both response and activity
+  all_cluster = permutations(n = C, r = N, repeats.allowed = T)
+  for (i in 1:5) {
+    epsilon_p = epsilon_p_seq[i]
+    for (j in 1:5) {
+      epsilon_mu = epsilon_mu_seq[j]
+      cutoff = qnorm(p0[1] + epsilon_p) - qnorm(p0[1])
+      cutoff2 = epsilon_mu
+      bayes_cluster = NULL
+      prob_rec = acti_rec = NULL 
+      for (k in 1:nrow(all_cluster)) {
+        group = all_cluster[k, ]
+        res = post(response, activity, ninter, group, cutoff, cutoff2, n.adapt, n.burn, n.iter)
+        this_prob = pnorm(0, mean = qnorm(p0) + res$mu1_rec, sd = 1, lower.tail = FALSE)
+        prob_rec = rbind(prob_rec, as.numeric(rowMeans(this_prob > p0) > reject_rate))
+        this_acti = mu0 + res$mu2_rec
+        acti_rec = rbind(acti_rec, as.numeric(rowMeans(this_acti > mu0) > reject_rate))
+        bayes_cluster = c(bayes_cluster, res$factor)# this the result vector of BF after iterating thru every permutation
+      }
+      index = which.max(bayes_cluster)
+      reject_prob = prob_rec[index, ]
+      reject_acti = acti_rec[index, ]
+      weak = reject_prob | reject_acti
+      weak1[i, j] = weak1[i, j] + weak[1]
+      weak2[i, j] = weak2[i, j] + weak[2]
+      weak3[i, j] = weak3[i, j] + weak[3]
+      weak4[i, j] = weak4[i, j] + weak[4]
+      strong = reject_prob & reject_acti
+      strong1[i, j] = strong1[i, j] + strong[1]
+      strong2[i, j] = strong2[i, j] + strong[2]
+      strong3[i, j] = strong3[i, j] + strong[3]
+      strong4[i, j] = strong4[i, j] + strong[4]
+    }
+  }
+  setTxtProgressBar(pb, m / M)
+}
+
+
+
+
+
+
