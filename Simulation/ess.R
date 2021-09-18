6c656092fa401433a83c8304694536063db34f82
Xiaoou Pan <xip024@ucsd.edu>
2021-09-18 13:34:15 -0700

    estimat ess

--- Simulation/ess.R
+++ Simulation/ess.R
@@ -4,37 +4,62 @@ library(tikzDevice)
 rm(list = ls())
 
 n = 5000
-p0 = 0.15
 m = 100
 var.pri = seq(2, 50, length.out = m)
-ess = rep(0, m)
-pb = txtProgressBar(style = 3)
+p0 = 0.15
+pa = p0 + 0.3
+ess1 = rep(0, m)
 for (i in 1:m) {
   w = rbinom(n, 1, 0.5)
-  mu = w * rnorm(n, 1, var.pri[i])
+  mu = w * rnorm(n, qnorm(pa) - qnorm(p0), var.pri[i])
   prob = pnorm(qnorm(p0) + mu)
   E = mean(prob)
   V = var(prob)
   a = (1 - E) * E^2 / V - E
   b = a * (1 - E) / E
-  ess[i] = a + b
-  setTxtProgressBar(pb, i / m)
+  ess1[i] = a + b
+}
+p0 = 0.12
+pa = p0 + 0.3
+ess2 = rep(0, m)
+for (i in 1:m) {
+  w = rbinom(n, 1, 0.5)
+  mu = w * rnorm(n, qnorm(pa) - qnorm(p0), var.pri[i])
+  prob = pnorm(qnorm(p0) + mu)
+  E = mean(prob)
+  V = var(prob)
+  a = (1 - E) * E^2 / V - E
+  b = a * (1 - E) / E
+  ess2[i] = a + b
+}
+p0 = 0.18
+pa = p0 + 0.3
+ess3 = rep(0, m)
+for (i in 1:m) {
+  w = rbinom(n, 1, 0.5)
+  mu = w * rnorm(n, qnorm(pa) - qnorm(p0), var.pri[i])
+  prob = pnorm(qnorm(p0) + mu)
+  E = mean(prob)
+  V = var(prob)
+  a = (1 - E) * E^2 / V - E
+  b = a * (1 - E) / E
+  ess3[i] = a + b
 }
 
-var.pri = rep(var.pri, 3)
-ess = c(ess, rep(0.5, m), rep(0.6, m))
+var.pri = rep(var.pri, 5)
+ess = c(ess1, ess2, ess3, rep(0.4, m), rep(0.6, m))
 dat = as.data.frame(cbind(var.pri, ess))
 colnames(dat) = c("var", "ess")
-dat$type = c(rep("ess", m), rep("lower", m),  rep("upper", m))
-dat$type = factor(dat$type, levels = c("ess", "lower", "upper"))
+dat$type = c(rep("ess1", m), rep("ess2", m), rep("ess3", m), rep("lower", m),  rep("upper", m))
+dat$type = factor(dat$type, levels = c("ess1", "ess2", "ess3", "lower", "upper"))
 
 setwd("~/Dropbox/Mayo-intern/Spike-and-slab")
 tikz("plot.tex", standAlone = TRUE, width = 7, height = 5)
 ggplot(dat, aes(x = var, y = ess, color = type)) +
   geom_line(aes(y = ess, color = type, linetype = type), size = 3) + 
-  scale_linetype_manual(values = c("solid", "dashed", "dashed")) +
+  scale_linetype_manual(values = c("solid", "solid", "solid", "dashed", "dashed")) +
   theme_bw() + xlab("Candidates of $\\sigma^2$") + 
-  ylab("Effective sample size") + 
+  ylab("Approximate effective sample size") + 
   theme(legend.position = "none", axis.text = element_text(size = 15), axis.title = element_text(size = 20))
 dev.off()
 tools::texi2dvi("plot.tex", pdf = T)
